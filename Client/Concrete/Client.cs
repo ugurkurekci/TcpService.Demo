2858f654baf7222203521fc45789bc7019c3983f
Kadir Akın <kadir.akin@yapidrom.com.tr>
2021-12-16 10:02:26 +0300

    fixed

--- Client/Concrete/Client.cs
+++ Client/Concrete/Client.cs
@@ -7,37 +7,44 @@ using System.Threading.Tasks;
 
 namespace Client.Concrete
 {
+   
     public class Client
     {
-        const int PORT_NO = 5000;
+        const int PORT_NO = 5000; //const proje kapsamında sabit hiç değişmicek.
         const string SERVER_IP = "127.0.0.1";
-
-
+        string textToSend;
         public void ClientPush()
         {
-            //---data to send to the server---
-            while (true)
+            try
             {
-                string textToSend;
-                Console.WriteLine("Not: ");
-                textToSend = Console.ReadLine();
+                while (true)
+                {
 
-                //---create a TCPClient object at the IP and port no.---
-                TcpClient client = new TcpClient(SERVER_IP, PORT_NO);
-                NetworkStream nwStream = client.GetStream();
-                byte[] bytesToSend = ASCIIEncoding.ASCII.GetBytes(textToSend);
+                    Console.Write("Not: ");
+                    textToSend = Console.ReadLine(); //---Servere Gönderilen veri girişi---//
 
-                //---send the text---
-                Console.WriteLine("Gönderilen Veri : " + textToSend);
-                nwStream.Write(bytesToSend, 0, bytesToSend.Length);
 
-                //---read back the text---
-                byte[] bytesToRead = new byte[client.ReceiveBufferSize];
-                int bytesRead = nwStream.Read(bytesToRead, 0, client.ReceiveBufferSize);
-                Console.WriteLine("Alınan Veri : " + Encoding.ASCII.GetString(bytesToRead, 0, bytesRead));
-                Console.ReadLine();
-                client.Close();
+                    TcpClient client = new TcpClient(SERVER_IP, PORT_NO);
+                    NetworkStream nwStream = client.GetStream();
+                    byte[] bytesToSend = ASCIIEncoding.ASCII.GetBytes(textToSend);
+                    //---send the text---
+                    Console.WriteLine("Gönderilen Veri : " + textToSend);
+                    nwStream.Write(bytesToSend, 0, bytesToSend.Length);
+                    //---read back the text---
+                    //byte[] bytesToRead = new byte[client.ReceiveBufferSize];
+                    //int bytesRead = nwStream.Read(bytesToRead, 0, client.ReceiveBufferSize);
+                    //Console.WriteLine("Alınan Veri : " + Encoding.ASCII.GetString(bytesToRead, 0, bytesRead));
+                    //Console.ReadLine();
+                    client.Close();
+                }
             }
+            catch (Exception)
+            {
+
+                Console.WriteLine("Hata var");
+            }
+
+
         }
     }
 }
--- Server/Concrete/Server.cs
+++ Server/Concrete/Server.cs
@@ -15,32 +15,50 @@ namespace Server.Concrete
         const string SERVER_IP = "127.0.0.1";
         public void Listener()
         {
-            //---listen at the specified IP and port no.---
-            IPAddress localAdd = IPAddress.Parse(SERVER_IP);
-            TcpListener listener = new TcpListener(localAdd, PORT_NO);
-            Console.WriteLine("Dinliyor...");
-            listener.Start();
+            try
+            {
+                while (true)
+                {
+                    //---listen at the specified IP and port no.---
+                    IPAddress localAdd = IPAddress.Parse(SERVER_IP);
+                    TcpListener listener = new TcpListener(localAdd, PORT_NO);
+                    Console.WriteLine("Dinliyor...");
+                    listener.Start();
+                    TcpClient client = listener.AcceptTcpClient();
 
-            //---incoming client connected---
-            TcpClient client = listener.AcceptTcpClient();
+                    //---get the incoming data through a network stream---
+                    NetworkStream nwStream = client.GetStream();
+                    byte[] buffer = new byte[client.ReceiveBufferSize];
 
-            //---get the incoming data through a network stream---
-            NetworkStream nwStream = client.GetStream();
-            byte[] buffer = new byte[client.ReceiveBufferSize];
+                    //---read incoming stream---
+                    int bytesRead = nwStream.Read(buffer, 0, client.ReceiveBufferSize);
 
-            //---read incoming stream---
-            int bytesRead = nwStream.Read(buffer, 0, client.ReceiveBufferSize);
+                    //---convert the data received into a string---
+                    string dataReceived = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                    Console.WriteLine("Alınan veri : " + dataReceived);
 
-            //---convert the data received into a string---
-            string dataReceived = Encoding.ASCII.GetString(buffer, 0, bytesRead);
-            Console.WriteLine("Alınan veri : " + dataReceived);
+                    ////---write back the text to the client---
+                    //Console.WriteLine("Geri Gönderme : " + dataReceived);
+                    //nwStream.Write(buffer, 0, bytesRead);
+                    client.Close();
+                    listener.Stop();
 
-            //---write back the text to the client---
-            Console.WriteLine("Geri Gönderme : " + dataReceived);
-            nwStream.Write(buffer, 0, bytesRead);
-            client.Close();
-            listener.Stop();
-            Console.ReadLine();
+
+                }
+
+                //---incoming client connected---
+
+                Console.ReadLine();
+
+
+
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("Hata var");
+            }
+
         }
     }
 }
